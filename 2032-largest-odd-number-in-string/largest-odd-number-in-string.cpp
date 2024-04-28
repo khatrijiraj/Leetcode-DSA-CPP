bd0c8b9768a463efe0a232ead8caf0e6c618633f
Raj Khatri <khatriraj030@gmail.com>
2024-04-28 15:54:06 +0530

    Time: 12 ms (99.56%) | Memory: 16.3 MB (25.31%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
+        std::ios_base::sync_with_stdio(0);
+        cin.tie(0);
         for (int i = num.length() - 1; i >= 0; i--) {
             if ((num[i] - '0') % 2 != 0) {
                 return num.substr(0, i + 1);
