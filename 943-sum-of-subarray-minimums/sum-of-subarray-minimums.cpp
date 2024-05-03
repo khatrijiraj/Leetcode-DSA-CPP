8b7a458a37f32f0cc449222458dc7b290b63686d
Raj Khatri <khatriraj030@gmail.com>
2024-05-03 22:30:48 +0530

    Time: 76 ms (21.68%) | Memory: 49.8 MB (5.71%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,40 @@
+int mod = 1e9 + 7;
+
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> leftOptions;
+        vector<int> rightOptions;
+        stack<pair<int, int>> stk;
+
+        for (int i = 0; i < arr.size(); i++) {
+            while (!stk.empty() && stk.top().first >= arr[i]) {
+                stk.pop();
+            }
+            leftOptions.push_back((stk.empty() ? i + 1 : i - stk.top().second));
+            stk.push({arr[i], i});
+        }
+
+        while (!stk.empty())
+            stk.pop();
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            while (!stk.empty() && stk.top().first > arr[i]) {
+                stk.pop();
+            }
+            rightOptions.push_back(
+                (stk.empty() ? n - i : stk.top().second - i));
+            stk.push({arr[i], i});
+        }
+        reverse(rightOptions.begin(), rightOptions.end());
+
+        long long res = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            long long curr =
+                (1ll * leftOptions[i] * rightOptions[i] * arr[i]) % mod;
+            res = (res + curr) % mod;
+        }
+        return (int)res;
+    }
+};
