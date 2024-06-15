2040e54f143c8898aec30b859d8b7de66b212348
Raj Khatri <khatriraj030@gmail.com>
2024-06-15 11:52:20 +0530

    Time: 120 ms (95.52%) | Memory: 85.5 MB (42.83%) - LeetSync

---
+++ 502-ipo/ipo.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits,
+                             vector<int>& capital) {
+        vector<pair<int, int>> projects;
+        for (int i = 0; i < profits.size(); i++) {
+            projects.emplace_back(capital[i], profits[i]);
+        }
+
+        sort(projects.begin(), projects.end());
+
+        priority_queue<int> maxProfitHeap;
+
+        int currentCapital = w;
+        int projectIndex = 0;
+
+        for (int i = 0; i < k; i++) {
+            while (projectIndex < projects.size() &&
+                   projects[projectIndex].first <= currentCapital) {
+                maxProfitHeap.push(projects[projectIndex].second);
+                ++projectIndex;
+            }
+
+            if (maxProfitHeap.empty()) {
+                break;
+            }
+
+            currentCapital += maxProfitHeap.top();
+            maxProfitHeap.pop();
+        }
+
+        return currentCapital;
+    }
+};
