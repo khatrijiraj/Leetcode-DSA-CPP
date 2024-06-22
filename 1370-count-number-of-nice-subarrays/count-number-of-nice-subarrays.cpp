1a727eea02f51c89ec02a3807587164a3e2d14e0
Raj Khatri <khatriraj030@gmail.com>
2024-06-22 16:41:51 +0530

    Time: 117 ms (21.29%) | Memory: 97 MB (7.67%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    Solution(){
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        vector<int> oddArray(nums.size());
+
+        for (int i = 0; i < nums.size(); i++) {
+            oddArray[i] = nums[i] % 2;
+        }
+
+        unordered_map<int, int> mpp;
+        mpp[0] = 1;
+
+        int current = 0;
+        int count = 0;
+
+        for (int it : oddArray) {
+            current += it;
+
+            if (mpp.find(current - k) != mpp.end()) {
+                count += mpp[current - k];
+            }
+
+            mpp[current]++;
+        }
+
+        return count;
+    }
+};
