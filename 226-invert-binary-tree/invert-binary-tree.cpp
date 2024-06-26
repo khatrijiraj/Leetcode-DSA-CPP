a429bd706994883834cf73bb6537a4d6dbfa0100
Raj Khatri <khatriraj030@gmail.com>
2024-06-26 21:38:21 +0530

    Time: 5 ms (27.49%) | Memory: 11.4 MB (16.74%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -11,6 +11,12 @@
  */
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+    }
+    
     TreeNode* invertTree(TreeNode* root) {
         if(!root){
             return NULL;
