4a1f29ea8fe6264591f132919dd35420e50c04f7
Raj Khatri <khatriraj030@gmail.com>
2024-06-26 23:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (16.68%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -5,9 +5,9 @@ public:
         cin.tie(0);
         cout.tie(0);
     }
-    
+
     int addDigits(int num) {
-        while (num >= 10) {
+        while (num/10) {
             int sum = 0;
             while (num > 0) {
                 sum += num % 10;
