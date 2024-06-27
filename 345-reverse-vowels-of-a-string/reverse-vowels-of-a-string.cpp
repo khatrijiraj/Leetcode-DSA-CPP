299b4d5bb1acd4edef95df6ce2205e94952790d9
Raj Khatri <khatriraj030@gmail.com>
2024-06-27 22:27:54 +0530

    Time: 4 ms (82.21%) | Memory: 9.5 MB (32.78%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+    }
+    
     string reverseVowels(string s) {
         int left = 0;
         int right = s.length() - 1;
