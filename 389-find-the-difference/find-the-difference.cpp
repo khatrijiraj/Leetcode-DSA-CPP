6890bdc8e4cb03e1d2678920f808df4ac97286bf
Raj Khatri <khatriraj030@gmail.com>
2024-06-27 23:37:48 +0530

    Time: 4 ms (49.16%) | Memory: 7.9 MB (31.96%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+    }
+    
     char findTheDifference(string s, string t) {
         char result = 0;
 
