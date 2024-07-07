4a63b592bbb0bee923dee3f5b8210fe0755ffe4f
Raj Khatri <khatriraj030@gmail.com>
2024-07-07 22:33:14 +0530

    Time: 221 ms (93.05%) | Memory: 92.4 MB (99.58%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         if (intervals.empty()) {
             return 0;
