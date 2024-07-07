b2b61eb03008b96f0a7be0376e575620d139f762
Raj Khatri <khatriraj030@gmail.com>
2024-07-07 11:03:10 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (95.58%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int totalDrank = 0;
+        int emptyBottles = 0;
+
+        while (numBottles > 0) {
+            totalDrank += numBottles;
+            emptyBottles += numBottles;
+            numBottles = emptyBottles / numExchange;
+            emptyBottles = emptyBottles % numExchange;
+        }
+
+        return totalDrank;
+    }
+};
