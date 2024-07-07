87ad63bd818c479c0e5bd91b814286757bcd2b7c
Raj Khatri <khatriraj030@gmail.com>
2024-07-07 18:28:31 +0530

    Time: 10 ms (90.50%) | Memory: 20.2 MB (32.46%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+
     int candy(vector<int>& ratings) {
         int n = ratings.size();
 
