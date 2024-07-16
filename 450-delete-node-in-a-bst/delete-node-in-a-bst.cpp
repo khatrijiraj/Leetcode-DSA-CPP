04c35eb30cbb0856f99e57bde23ff692dda162c4
Raj Khatri <khatriraj030@gmail.com>
2024-07-16 23:15:04 +0530

    Time: 22 ms (68.55%) | Memory: 33.8 MB (5.27%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -23,6 +23,12 @@ private:
     }
 
 public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+    }
+    
     TreeNode* deleteNode(TreeNode* root, int key) {
         if (!root) {
             return nullptr;
