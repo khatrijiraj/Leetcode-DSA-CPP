8ecd2425c86158bf1cc3e840b7820764c104618a
Raj Khatri <khatriraj030@gmail.com>
2024-07-21 15:22:50 +0530

    Time: 120 ms (13.80%) | Memory: 65.1 MB (38.31%) - LeetSync

---
+++ 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.cpp
@@ -0,0 +1,65 @@
+class Solution {
+private:
+    vector<int> topologicalSort(vector<vector<int>>& conditions, int& k) {
+        vector<int> indegree(k + 1, 0);
+        vector<vector<int>> graph(k + 1);
+
+        for (auto it : conditions) {
+            int u = it[0];
+            int v = it[1];
+            graph[u].push_back(v);
+            ++indegree[v];
+        }
+
+        queue<int> qt;
+
+        for (int i = 1; i <= k; i++) {
+            if (indegree[i] == 0) {
+                qt.push(i);
+            }
+        }
+
+        vector<int> result;
+        while (!qt.empty()) {
+            int node = qt.front();
+            qt.pop();
+            result.push_back(node);
+
+            for (auto it : graph[node]) {
+                if (--indegree[it] == 0) {
+                    qt.push(it);
+                }
+            }
+        }
+
+        if (result.size() == k) {
+            return result;
+        } else {
+            return vector<int>{};
+        }
+    }
+
+public:
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions,
+                                    vector<vector<int>>& colConditions) {
+        vector<int> rowOrder = topologicalSort(rowConditions, k);
+        vector<int> colOrder = topologicalSort(colConditions, k);
+
+        if (rowOrder.empty() || colOrder.empty()) {
+            return {};
+        }
+
+        vector<vector<int>> result(k, vector<int>(k, 0));
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                if (rowOrder[i] == colOrder[j]) {
+                    result[i][j] = rowOrder[i];
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+};
