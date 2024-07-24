35c1bdc560ca7312b89b7e4dd21ea70034e30149
Raj Khatri <khatriraj030@gmail.com>
2024-07-24 12:12:30 +0530

    Time: 129 ms (96.54%) | Memory: 87.5 MB (79.65%) - LeetSync

---
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        vector<pair<int, int>> converted;
+        for (int i = 0; i < nums.size(); i++) {
+            int temp = nums[i];
+            int sum = 0;
+            int place = 1;
+
+            if(temp == 0){
+                sum = mapping[0];
+            }
+            while (temp) {
+                int last = temp % 10;
+                sum += place * (mapping[last]);
+                temp /= 10;
+                place *= 10;
+            }
+            converted.push_back({sum, i});
+        }
+
+        sort(converted.begin(), converted.end());
+
+        vector<int> result;
+        for (auto& it : converted) {
+            result.push_back(nums[it.second]);
+        }
+
+        return result;
+    }
+};
