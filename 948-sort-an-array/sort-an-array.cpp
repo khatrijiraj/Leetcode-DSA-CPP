12adf28d0f4c07884f4b4f85a4f8ed4bf44c4e82
Raj Khatri <khatriraj030@gmail.com>
2024-07-25 12:30:54 +0530

    Time: 352 ms (38.25%) | Memory: 179.9 MB (23.10%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+
     vector<int> sortArray(vector<int>& nums) {
         if (nums.empty()) {
             return nums;
