6eec7bd007da20f390c32e44ac6b28ed257fc636
Raj Khatri <khatriraj030@gmail.com>
2024-08-04 23:11:53 +0530

    Time: 3 ms (84.13%) | Memory: 16.3 MB (33.71%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+
     int orangesRotting(vector<vector<int>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
