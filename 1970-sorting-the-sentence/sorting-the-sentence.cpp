16f699d46419688f9b8dd1afc4ddb0d6656ac47f
Raj Khatri <khatriraj030@gmail.com>
2024-08-27 13:07:48 +0530

    Time: 3 ms (43.92%) | Memory: 8 MB (11.65%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.cpp
+++ 1970-sorting-the-sentence/sorting-the-sentence.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+
     string sortSentence(string s) {
         map<int, string> mpp;
         int index = 0;
         string temp = "";
-        while (index < s.size()) {
-            if (s[index] == ' ') {
+        while (index <= s.size()) {
+            if (s[index] == '\0' || s[index] == ' ') {
                 int last = temp.back();
                 temp.pop_back();
                 mpp[last] = temp;
@@ -15,10 +21,10 @@ public:
             }
             index++;
         }
-        int last = temp.back();
-        temp.pop_back();
-        mpp[last] = temp;
-        
+        // int last = temp.back();
+        // temp.pop_back();
+        // mpp[last] = temp;
+
         string result = "";
 
         for (auto it : mpp) {
