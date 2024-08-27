0b7da3844e92c704403a6d6b122c946460679b19
Raj Khatri <khatriraj030@gmail.com>
2024-08-27 13:03:34 +0530

    Time: 3 ms (43.92%) | Memory: 7.9 MB (22.58%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        map<int, string> mpp;
+        int index = 0;
+        string temp = "";
+        while (index < s.size()) {
+            if (s[index] == ' ') {
+                int last = temp.back();
+                temp.pop_back();
+                mpp[last] = temp;
+                temp = "";
+            } else {
+                temp += s[index];
+            }
+            index++;
+        }
+        int last = temp.back();
+        temp.pop_back();
+        mpp[last] = temp;
+        
+        string result = "";
+
+        for (auto it : mpp) {
+            result += it.second + " ";
+        }
+        result.pop_back();
+        return result;
+    }
+};
