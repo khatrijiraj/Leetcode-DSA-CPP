e194c19f9d012b8f2ebdc52a505bb7bdc022a349
Raj Khatri <khatriraj030@gmail.com>
2025-01-01 11:53:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (38.26%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int maxScore(string s) {
+        /*
+
+        Timecomplexity:  O(N^2) solution
+
+        Simply splitting string in two parts and counting zeroinleft &
+        oneinright then keeping a track of maximum score we seen in all possible
+        splits:
+
         int maxi = 0;
         for (int i = 0; i < s.length() - 1; i++) {
             int curr = 0;
@@ -17,5 +25,51 @@ public:
             maxi = max(maxi, curr);
         }
         return maxi;
+
+        */
+
+        /*
+
+        OPTIMAL APPROACH ONE PASS SOLUTION:
+        Timecomplexity: O(N) solution
+
+        score = zeroinleft + oneinright
+
+        but oneinright = totalones - oneinleft
+
+        substituting this in equation 1:
+        score = zeroinleft + totalone - oneinleft
+
+        now we know totalone is constant so we just need to find a maximum
+        value of:
+
+        zeroinleft - oneinleft
+
+        which only focuses on left part
+
+        thus its a one pass solution
+
+        */
+
+        int zeros = 0;
+        int ones = 0;
+
+        int best = INT_MIN;
+
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] == '1') {
+                ones++;
+            } else {
+                zeros++;
+            }
+
+            best = max(best, zeros - ones);
+        }
+
+        if (s[s.size() - 1] == '1') {
+            ones++;
+        }
+
+        return best + ones;
     }
 };
