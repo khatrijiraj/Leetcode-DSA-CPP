6d22ef2120093c12d212a85f6557ae4f66a9a2e4
Raj Khatri <khatriraj030@gmail.com>
2025-01-05 13:48:09 +0530

    Time: 2 ms (81.15%) | Memory: 14.9 MB (26.38%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         /* brute approach
-                       for (int i = 0; i < nums.size(); i++) {
-                           for (int j = i + 1; j < nums.size(); j++) {
-                               if ((nums[i] + nums[j]) == target) {
-                                   return {i, j};
-                               }
-                           }
-                       }
-                       return {-1, -1}; */
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {-1, -1}; */
 
-        /*
-        better approach
-         */
+        // optimal approach
         unordered_map<int, int> mpp;
         for (int i = 0; i < nums.size(); i++) {
             int req = target - nums[i];
@@ -24,8 +22,26 @@ public:
         }
         return {-1, -1};
 
-        /* best solution
+        /* OPTIONAL approach
+        if we do not want to store in map
 
+
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        int j = nums.size() - 1;
+
+        while (i < j) {
+            int currsum = nums[i] + nums[j];
+            if (currsum == target) {
+                return {i, j};
+            } else if (currsum < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return {-1, -1};
          */
     }
 };
