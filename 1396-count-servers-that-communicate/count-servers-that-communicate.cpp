819b92a9620a3f2a2a3f40b72ed6502f35970ae1
Raj Khatri <khatriraj030@gmail.com>
2025-01-23 20:13:08 +0530

    Time: 7 ms (31.23%) | Memory: 26.7 MB (39.90%) - LeetSync

---
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> rowSum(m, 0);
+        vector<int> colSum(n, 0);
+
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    rowSum[i]++;
+                    colSum[j]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && (rowSum[i] > 1 || colSum[j] > 1)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
