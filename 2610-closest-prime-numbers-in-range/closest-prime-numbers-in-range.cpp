7dc4a0abb61c01e5c7acc7ad7318e67e4ab5f424
Raj Khatri <khatriraj030@gmail.com>
2025-03-07 15:10:08 +0530

    Time: 1093 ms (5.18%) | Memory: 8.1 MB (98.33%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,6 +1,6 @@
 class Solution {
     bool checkprime(int num) {
-        if (num == 1) {
+        if (num < 2) {
             return false;
         }
         for (int i = 2; i <= sqrt(num); i++) {
@@ -12,32 +12,54 @@ class Solution {
     }
 
     vector<int> PrimeRange(int left, int right) {
-        vector<int> result;
+        vector<int> primes;
         for (int i = left; i <= right; i++) {
             if (checkprime(i)) {
-                result.push_back(i);
+                primes.push_back(i);
             }
         }
-        return result;
+        return primes;
     }
 
 public:
     vector<int> closestPrimes(int left, int right) {
-        vector<int> result = PrimeRange(left, right);
+        /* vector<int> primes = PrimeRange(left, right);
 
         int mini = INT_MAX;
-        if (result.size() < 2) {
+        if (primes.size() < 2) {
             return {-1, -1};
         }
 
         int x = -1, y = -1;
 
-        for (int i = 0; i < result.size() - 1; i++) {
-            int temp = result[i + 1] - result[i];
+        for (int i = 0; i < primes.size() - 1; i++) {
+            int temp = primes[i + 1] - primes[i];
             if (temp < mini) {
                 mini = temp;
-                x = result[i];
-                y = result[i + 1];
+                x = primes[i];
+                y = primes[i + 1];
+            }
+        }
+
+        return {x, y}; */
+
+        int mini = INT_MAX;
+        int x = -1;
+        int y = -1;
+
+        int prevprime = -1;
+
+        for (int num = left; num <= right; num++) {
+            if (checkprime(num)) {
+                if (prevprime != -1) {
+                    int diff = num - prevprime;
+                    if (diff < mini) {
+                        mini = diff;
+                        x = prevprime;
+                        y = num;
+                    }
+                }
+                prevprime = num;
             }
         }
 
