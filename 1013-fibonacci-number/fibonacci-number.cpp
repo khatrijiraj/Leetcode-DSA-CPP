2c03ff5446091433220e6e8f5be042d266d6680b
Raj Khatri <khatriraj030@gmail.com>
2025-03-18 17:51:26 +0530

    Time: 3 ms (67.15%) | Memory: 8.4 MB (9.96%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,20 +1,53 @@
 class Solution {
-private:
-    int solve(int n, vector<int>& dp) {
-        if (n <= 1) {
-            return n;
-        }
+    /* private:
+        int solve(int n, vector<int>& dp) {
+            if (n <= 1) {
+                return n;
+            }
 
-        if (dp[n] != -1) {
-            return dp[n];
-        }
+            if (dp[n] != -1) {
+                return dp[n];
+            }
 
-        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-    }
+            return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+        } */
 
 public:
     int fib(int n) {
+        /* SIMPLE RECURSION
+        TC:- O(2^n)
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+         */
+
+        /* Memoized Recursion:- TOP DOWN APPROACH
         vector<int> dp(n + 1, -1);
-        return solve(n, dp);
+        return solve(n, dp); */
+
+        /* BOTTOM UP APPROACH
+        TC O(n)
+        */
+
+        if (n < 2) {
+            return n;
+        }
+
+        vector<int> dp(n + 1, -1);
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 };
