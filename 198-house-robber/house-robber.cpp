80555211748a265e08a83e5b01d464c1ee250597
Raj Khatri <khatriraj030@gmail.com>
2025-03-18 23:56:54 +0530

    Time: 2 ms (7.48%) | Memory: 10.6 MB (61.63%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 private:
     int solve(vector<int>& nums, int i, vector<int>& dp) {
@@ -19,5 +20,39 @@ public:
     int rob(vector<int>& nums) {
         vector<int> dp(nums.size() + 1, -1);
         return solve(nums, 0, dp);
+    }
+};
+*/
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        // dp[i] = max stolen money till ith house
+        vector<int> dp(n + 1, 0);
+
+        // no house nothing stolen, i = 0;
+        dp[0] = 0;
+
+        // only house then only one stolen, i = 1
+        dp[1] = nums[0];
+
+        for (int i = 2; i <= n; i++) {
+            // we have only two choices to make for each house either steal or
+            // skip from a house
+
+            // if we steal then current + house before adjacent maximum
+            int stolen = nums[i - 1] + dp[i - 2];
+
+            // if we are not stealing from current house then max from earlier
+            // house encountered
+            int skip = dp[i - 1];
+
+            // storing the maximum possibility
+            dp[i] = max(stolen, skip);
+        }
+
+        return dp[n];
     }
 };
